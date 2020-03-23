abda2c58aed9d8cc20a33d71585023ed91f6c839
Mario M <muellma.gruena@googlemail.com>
2020-03-23 16:49:16 +0100

    second commit

--- server.js
+++ server.js
@@ -109,6 +109,10 @@ app.get('/pagecount', function (req, res) {
   }
 });
 
+app.get('/hello', function (req, res) {
+  res.send('Hallo Mario');
+}
+
 // error handling
 app.use(function(err, req, res, next){
   console.error(err.stack);
